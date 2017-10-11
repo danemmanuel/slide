91bc57d0c01d9c1b1192fa9afd9268a0a50155a6
danemmanuel <danemmanuelturibio@gmail.com>
2017-10-11 17:34:22 -0300

    alteracc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-$(".teste").click(function(){
-	alert("teste");
+$(".tog1").click(function(){
+	$(".overlaytog1").toggleClass("viewoverley");
+})
+$(".overlaytog1").click(function(){
+	$(this).toggleClass("viewoverley");
+})
+
+
+$(".tog2").click(function(){
+	$(".overlaytog2").toggleClass("viewoverley");
+})
+$(".overlaytog2").click(function(){
+	$(this).toggleClass("viewoverley");
+})
+
+$(".tog3").click(function(){
+	$(".overlaytog3").toggleClass("viewoverley");
+})
+$(".overlaytog3").click(function(){
+	$(this).toggleClass("viewoverley");
+})
+
+$(".tog4").click(function(){
+	$(".overlaytog4").toggleClass("viewoverley");
+})
+$(".overlaytog4").click(function(){
+	$(this).toggleClass("viewoverley");
 })
